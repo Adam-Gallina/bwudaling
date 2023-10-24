cc324949714b4245992f2ae369449dd8fb443949
Adam Gallina <adamg461@gmail.com>
2023-10-23 18:42:09 -0700

    Added cwoc model

--- Assets/Scripts/Avatars/Projectiles/SmackProjectile.cs
+++ Assets/Scripts/Avatars/Projectiles/SmackProjectile.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class SmackProjectile : RicochetProjectile
 {
+    [SerializeField] private RangeF spinSpeed;
+
+    private void Start()
+    {
+        rb.AddTorque(new Vector3(0, Mathf.Deg2Rad * spinSpeed.RandomVal, 0) * Mathf.Sign(Random.Range(-1, 1)), ForceMode.VelocityChange);
+    }
+
     [Server]
     protected override void OnHitTarget(Collider other)
     {
